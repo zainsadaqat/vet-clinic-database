827b5daa36cbb81a57283a000d48d4ee886de186
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-14 03:12:53 +0500

    Alter animals table

--- schema.sql
+++ schema.sql
@@ -1,21 +1,32 @@
 /* Database schema to keep the structure of entire database. */
 
+
+-------------------------------- DAY 01 --------------------------------
+
+
 -- Create a database named vet_clinic.
 CREATEDB VET_CLINIC;
 
--- Create a table named animals with the following columns:
--- id: integer
--- name: string
--- date_of_birth: date
--- escape_attempts: integer
--- neutered: boolean
--- weight_kg: decimal
 
+-- Create a table named animals with the following columns:
 VET_CLINIC=# CREATE TABLE ANIMALS(
+-- id: integer
     ID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+-- name: string
     NAME TEXT,
+-- date_of_birth: date
     DATE_OF_BIRTH DATE,
+-- escape_attempts: integer
     ESCAPE_ATTEMPTS INT,
+-- neutered: boolean
     NEUTERED BOOLEAN,
+-- weight_kg: decimal
     WEIGHT_KG DECIMAL
 );
+
+
+-------------------------------- DAY 02 --------------------------------
+
+
+-- Add a column species of type string to your animals table. Modify your schema.sql file.
+ALTER TABLE ANIMALS ADD SPECIES TEXT;
