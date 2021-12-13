3ef6c6850dfac8be994327c12b7bcc8a2f79cf0f
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-13 21:07:23 +0500

    Create database and table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,21 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+-- Create a database named vet_clinic.
+CREATEDB VET_CLINIC;
+
+-- Create a table named animals with the following columns:
+-- id: integer
+-- name: string
+-- date_of_birth: date
+-- escape_attempts: integer
+-- neutered: boolean
+-- weight_kg: decimal
+
+VET_CLINIC=# CREATE TABLE ANIMALS(
+    ID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    NAME TEXT,
+    DATE_OF_BIRTH DATE,
+    ESCAPE_ATTEMPTS INT,
+    NEUTERED BOOLEAN,
+    WEIGHT_KG DECIMAL
 );
