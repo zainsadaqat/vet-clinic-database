ff300a1956572be64d60883cecbd246bea476b39
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-14 05:37:06 +0500

    Create new tables

--- schema.sql
+++ schema.sql
@@ -19,3 +19,45 @@ VET_CLINIC=# CREATE TABLE ANIMALS(
     NEUTERED BOOLEAN,
     WEIGHT_KG DECIMAL
 );
+
+
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer
+CREATE TABLE OWNERS(
+    ID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    FULL_NAME TEXT,
+    AGE INT
+);
+
+
+-- Create a table named species with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+CREATE TABLE SPECIES(
+    ID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    NAME TEXT
+);
+
+
+-- Modify animals table:
+
+-- Remove column species
+ALTER TABLE ANIMALS DROP COLUMN SPECIES;
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE ANIMALS ADD OWNER_ID INT;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE ANIMALS ADD SPECIES_ID INT;
+
+
+-- Modify your inserted animals so it includes the species_id value:
+
+-- If the name ends in "mon" it will be Digimon
+UPDATE ANIMALS SET SPECIES_ID = (SELECT ID FROM SPECIES WHERE NAME='Digimon') WHERE NAME LIKE '%mon';
+
+-- All other animals are Pokemon
+UPDATE ANIMALS SET SPECIES_ID = (SELECT ID FROM SPECIES WHERE NAME='Pokemon') WHERE NAME NOT LIKE '%mon';
+
