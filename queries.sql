de21307e1a5b7ae7e1eba04be97145025fd7bada
Maximiliano Diaz <87093942+mistermaxis@users.noreply.github.com>
2021-12-21 17:21:43 -0300

    Update queries.sql
    Test performance of provided queries

--- queries.sql
+++ queries.sql
@@ -177,3 +177,8 @@ WHERE vets.name='Vet Maisy Smith'
 GROUP BY species.name
 ORDER BY COUNT DESC
 LIMIT 1;
+
+-- Test performance of provided queries with explain analyze
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
